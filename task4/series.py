c2bf3a2fd933c4e79ad0d851b21898225729705a
borisovvilyaa <borisovvilyaa@gmail.com>
2023-05-25 16:04:26 +0300

    fix task

--- task4/series.py
+++ task4/series.py
@@ -1,53 +1,71 @@
 import random
 
-def count_zero_series(sequence) -> list:
+def count_series_length(sequence) -> list:
     """
-    Count the number of series of consecutive zeros of different lengths in the given sequence.
-
-    @param sequence: The input sequence of binary digits.
-    @return: A list containing the counts of series of different lengths.
+    Count the series lengths of consecutive 0s and 1s in a given sequence.
+    
+    @param sequence: The input sequence of bits.
+    @type sequence: str
+    
+    @return: A dictionary containing the count of series for each length.
+    @rtype: dict
     """
-    series_counts = [0, 0, 0, 0, 0, 0]  # Initialize counters for each series length
-    current_series_length = 0  # Variable to track the current series length
+    series_lengths = {'0': [0]*7, '1': [0]*7}  # Stores the count of series for each length
+    
+    current_char = sequence[0]  # Initial character of the sequence
+    current_length = 1  # Initial length of the series
 
-    for number in sequence:
-        if number == '1':  # If a '1' is encountered, reset the current series length
-            current_series_length = 0
-        else:
-            current_series_length += 1  # If a '0' is encountered, increase the current series length
-            if current_series_length >= 6:  # If the current series length is >= 6, increment the counter for series of length 6 or more
-                series_counts[5] += 1
-            else:
-                series_counts[current_series_length - 1] += 1  # Increment the counter for the corresponding series length
+    for i in range(1, len(sequence)):
+        if sequence[i] == current_char:  # If the current character matches the previous one
+            current_length += 1
+        else:  # If the current character is different from the previous one
+            series_lengths[current_char][min(current_length, 6)] += 1
+            current_char = sequence[i]
+            current_length = 1
 
-    return series_counts
+    # Update the count of series for the last character in the sequence
+    series_lengths[current_char][min(current_length, 6)] += 1
+    
+    return series_lengths
+    
 
-
-def test_sequence(sequence) -> bool:
+def test_random_sequence(sequence) -> bool:
     """
-    Test the given sequence for randomness based on the counts of zero series.
-
-    @param sequence: The input sequence of binary digits.
-    @return: True if the sequence meets the randomness criteria, False otherwise.
+    Test if a given sequence meets the randomness criteria based on the count of series.
+    
+    @param sequence: The input sequence of bits.
+    @type sequence: str
+    
+    @return: True if the sequence is considered random, False otherwise.
+    @rtype: bool
     """
-    series_counts = count_zero_series(sequence)
-    print(series_counts)
-    # Check if all series counts fall within the specified ranges
-    return (2267 <= series_counts[0] <= 2733 and
-            1079 <= series_counts[1] <= 1421 and
-            502 <= series_counts[2] <= 748 and
-            223 <= series_counts[3] <= 402 and
-            90 <= series_counts[4] <= 223 and
-            90 <= series_counts[5] <= 223)
+    series_lengths = count_series_length(sequence)
+    # Check if the count of series falls within the appropriate ranges
+    if (
+        2267 <= series_lengths['0'][1] <= 2733 and
+        1079 <= series_lengths['0'][2] <= 1421 and
+        502 <= series_lengths['0'][3] <= 748 and
+        223 <= series_lengths['0'][4] <= 402 and
+        90 <= series_lengths['0'][5] <= 223 and
+        90 <= series_lengths['0'][6] <= 223 and
+        2267 <= series_lengths['1'][1] <= 2733 and
+        1079 <= series_lengths['1'][2] <= 1421 and
+        502 <= series_lengths['1'][3] <= 748 and
+        223 <= series_lengths['1'][4] <= 402 and
+        90 <= series_lengths['1'][5] <= 223 and
+        90 <= series_lengths['1'][6] <= 223
+    ):
+        return True  
+    else:
+        return False
 
 
-# Generate a random sequence
+# Generating a sequence of bits with a length of 20,000
 sequence = ''.join(random.choices('01', k=20000))
 
-# Test the sequence
-result = test_sequence(sequence)
+# Performing the maximum series length test
+result = test_random_sequence(sequence)
 
-# Output the result
 if result:
     print("The sequence is considered random.")
 else:
