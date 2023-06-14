c878950462134758aad2ea5dab67e8d004f1b612
borisovvilyaa <borisovvilyaa@gmail.com>
2023-06-14 20:49:56 +0300

    add first task

--- task6/main.py
+++ task6/main.py
@@ -1,95 +1,102 @@
 from random import randint
-from sha1 import sha1
+import hashlib
+
 class ElGamal:
     def __init__(self) -> None:
-        #parameters
-        self.p = self.random_number()
-        self.g = self.find_primitive_root()
-        
-        #First Person
-        self.a = randint(1, self.p-1)
-        self.b = (self.g**self.a) % self.p
-        
-        #Signing the message
-        self.k = randint(1, self.p-1)
-        self.r = (self.g**self.k)%self.p
-        self.m = 10 # условно, це буде геш, бо в мене геш - це стрінг, а повинен бути для self.s інт
-        self.s = ((self.m - self.a*self.r)*self.k**(-1))%(self.p-1)
-        
-        #check signature 
-        self.y = (self.b**(-1))%self.p
-        self.u1 = (self.m*self.s**(-1))%(self.p-1)
-        self.u2 = (self.r*self.s**(-1))%(self.p-1)
-        self.v = (self.g**self.u1 * self.y ** self.u2) % self.p
-        
-    def get_parameter(self):
-        return self.p, self.g
-    def firstPerson(self):
-        return self.a, self.b
-    def get_signature(self):
-        return self.k, self.r, self.s
-    
-    def check(self):
-        return self.v == self.r, self.v, self.r
-    # def secondPerson(self):
-    #     return self.m, self.k, self.x
-    def random_number(self) -> int:
-        while True:
-            p = randint(2048, 4096)
-            if self.is_prime(p):
-                return p
-            
-    def find_primitive_root(self):
-        # Calculate Euler's totient function value (phi)
-        phi = self.p - 1
-        
-        # Factorize phi into prime factors
-        factors = self.factorize(phi)
-        
-        # Iterate over numbers from 1 to p-1
-        for g in range(1, self.p):
-            # Assume g is a primitive root
-            is_primitive_root = True
-            
-            # Check if g raised to the power of (phi // factor) modulo p is equal to 1
-            # for any factor in the factors list
-            for factor in factors:
-                if pow(g, phi // factor, self.p) == 1:
-                    # If the condition is met, g is not a primitive root
-                    is_primitive_root = False
-                    break
-            
-            # If g is a primitive root, return it
-            if is_primitive_root:
-                return g
-        
-        # If no primitive root is found, raise a ValueError
-        raise ValueError("The first-order root for the number p could not be found.")
+        self.p = self.generate_p()
+        self.g = self.find_primitive_root(self.p)
+        self.a = self.generate_private_key()
+        self.b = pow(self.g, self.a, self.p)
 
-    
-    def factorize(self, n):
-        factors = []
-        divisor = 2
-        while divisor * divisor <= n:
-            if n % divisor == 0:
-                factors.append(divisor)
-                n //= divisor
-            else:
-                divisor += 1
-        if n > 1:
-            factors.append(n)
-        return factors
+    def getParameters(self):
+        return self.p, self.g, self.a, self.b
 
-    def is_prime(self, x) -> bool:
-        if x < 2:
+    def is_prime(self, number):
+        if number < 2:
             return False
-        for i in range(2, (x//2)+1):
-            if x % i == 0:
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
                 return False
         return True
-o = ElGamal()
-print("p %s\ng %s\n" % o.get_parameter())
-print("Private Key %s\nPublic Key %s\n" %o.firstPerson())
-print("k %s (only for debug)\nr %s\ns %s\n" % o.get_signature())
-print("Check %s\nv %s\ns %s" % o.check())
-# print("'m' is %s\n'k' is %s\n'x' is %s"%o.secondPerson())
+
+    def find_primitive_root(self, p):
+        for g in range(2, p):
+            if self.is_primitive_root(g, p):
+                return g
+
+    def is_primitive_root(self, g, p):
+        elements = set()
+        for i in range(1, p):
+            elements.add(pow(g, i, p))
+        for i in range(1, p):
+            if i not in elements:
+                return False
+        return True
+
+    def generate_p(self):
+        p = randint(2048, 4096)
+        while True:
+            if self.is_prime(p):
+                return p
+            else:
+                p = randint(2048, 4096)
+
+    def generate_private_key(self):
+        a = randint(1, self.p - 1)
+        return a
+
+    def sign_message(self, message):
+        k = randint(1, self.p - 1)
+        r = pow(self.g, k, self.p)
+
+        h = self.hash_message(message)
+        inverse_k = self.mod_inverse(k, self.p - 1)
+        s = ((h - self.a * r) * inverse_k) % (self.p - 1)
+
+        return r, s
+
+    def hash_message(self, message):
+        hashed_message = hashlib.sha256(message.encode()).digest()
+        h = int.from_bytes(hashed_message, byteorder='big')
+        return h
+
+    def mod_inverse(self, a, m):
+        g, x, y = self.extended_gcd(a, m)
+        if g != 1:
+            raise ValueError("Неможливо знайти обернений елемент")
+        return x % m
+
+    def extended_gcd(self, a, b):
+        if a == 0:
+            return b, 0, 1
+        else:
+            g, x, y = self.extended_gcd(b % a, a)
+            return g, y - (b // a) * x, x
+
+    def verify_signature(self, message, r, s):
+        h = self.hash_message(message)
+        inverse_s = self.mod_inverse(s, self.p - 1)
+
+        u1 = (h * inverse_s) % (self.p - 1)
+        u2 = (r * inverse_s) % (self.p - 1)
+
+        y = self.mod_inverse(self.b, self.p)
+        v = (pow(self.g, u1, self.p) * pow(y, u2, self.p)) % self.p
+
+        return v == r
+
+el = ElGamal()
+p, g, a, b = el.getParameters()
+print("p: %s" % p)
+print("g: %s" % g)
+print("a: %s" % a)
+print("b: %s" % b)
+
+message = "Hello, world!"
+r, s = el.sign_message(message)
+print("Підпис повідомлення:")
+print("r: %s" % r)
+print("s: %s" % s)
+
+valid_signature = el.verify_signature(message, r, s)
+print("Перевірка підпису: %s" % valid_signature)
